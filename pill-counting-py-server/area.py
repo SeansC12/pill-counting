cc151177c660b2565030241328f6ef159a377a65
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2024-10-10 15:11:09 +0800

    used median instead

--- pill-counting-py-server/area.py
+++ pill-counting-py-server/area.py
@@ -40,13 +40,13 @@ def find_damaged_pills_by_area(counting_predictions, area_threshold, image):
         areas_rounded.append(ROUND_BASE * round(area / ROUND_BASE))
     
     print(areas, fake_areas)
-    mode = stats.mode(areas_rounded)
+    median = np.median(areas_rounded)
 
     for pill in counting_predictions:
         if pill["is_added"] or pill["is_damaged"]: continue
 
         area = calculate_area(image, pill["x"], pill["y"], pill["width"], pill["height"])
-        if abs(area - mode[0]) > (0.5 * mode[0]):
+        if abs(area - median) > (0.5 * median):
             pill["is_damaged"] = True
             pill["damaged_index"] = 1
             pill["damaged_signature"] = "Area too different from the mode."
